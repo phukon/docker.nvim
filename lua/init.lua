0abd80bdeffd7160afaca98ac6bc0285aa3c0c58
phukon <hello@rikiphukon.com>
2024-12-30 18:16:50 +0530

    feat: run docker image selection

--- lua/init.lua
+++ lua/init.lua
@@ -1,8 +1,10 @@
 local pickers = require('telescope.pickers')
 local finders = require('telescope.finders')
-local previewers = require('telescope.previewers')
-local utils = require('telescope.previewers.utils')
 local config = require('telescope.config').values
+local previewers = require('telescope.previewers')
+local actions = require('telescope.actions')
+local actions_state = require('telescope.actions.state')
+local utils = require('telescope.previewers.utils')
 
 local logger = require('plenary.log'):new()
 logger.level = 'debug'
@@ -26,7 +28,7 @@ M.show_docker_images = function (opts)
       entry_maker = function (entry)
         local parsed = vim.json.decode(entry)
         logger.debug(parsed)
-        if parsed then  
+        if parsed then
           return {
             value = parsed,
             display = parsed.Repository,
@@ -66,6 +68,27 @@ M.show_docker_images = function (opts)
         utils.highlighter(self.state.bufnr, "markdown")
       end
     }),
+
+    attach_mappings = function (prompt_bufnr)
+      actions.select_default:replace(function ()
+        local selection = actions_state.get_selected_entry()
+        actions.close(prompt_bufnr) -- closing the telescope window
+
+        logger.debug("📌 Selected", selection)
+
+        local command = {
+          "edit",
+          "term://docker",
+          "run",
+          "-it",
+          selection.value.Repository
+        }
+
+        vim.cmd(vim.fn.join(command, " "))
+        
+      end)
+      return true
+    end
 
   }):find()
 end
